566247abb6c42ad2e57abf2e97c3ab3ac2fff558
Niamh <niamh.linguist@gmail.com>
2024-11-17 00:14:05 +0200

    ceil and floor and die func

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -1,6 +1,9 @@
 extends CharacterBody2D
 
-const GRAVITY: float = 600.0
+const GRAVITY: float = 1000.0
+const POWER: float = -350.0
+
+@onready var anim_sprite: AnimatedSprite2D = $AnimSprite
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,4 +15,17 @@ func _physics_process(delta: float) -> void:
 	
 	velocity.y += GRAVITY * delta
 	
+	fly()
+	
 	move_and_slide()
+	
+	if is_on_floor() == true:
+		die()
+	
+func fly() -> void:
+	if Input.is_action_just_pressed("fly") == true:
+		velocity.y = POWER
+
+func die() -> void:
+	anim_sprite.stop()
+	set_physics_process(false)
