eaf4ca011cd96a21c5750109f5af856e2930c1ce
Niamh <niamh.linguist@gmail.com>
2024-11-17 14:03:28 +0200

    pipes scrolling

---
+++ scenes/pipes/pipes.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const SPEED: float = 120.0
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x -= delta * SPEED
+	check_off_screen()
+
+func check_off_screen() -> void:
+	if position.x < -500.0:
+		queue_free()
+
+func _on_screen_exited() -> void:
+	queue_free()
