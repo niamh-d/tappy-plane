a0fe2b4961f6d680a315bbbc54ad53a9d5ff387d
Niamh <niamh.linguist@gmail.com>
2024-11-17 12:01:46 +0200

    plane rotation animation

--- scenes/plane/plane.gd
+++ scenes/plane/plane.gd
@@ -4,6 +4,7 @@ const GRAVITY: float = 1000.0
 const POWER: float = -350.0
 
 @onready var anim_sprite: AnimatedSprite2D = $AnimSprite
+@onready var anim_player: AnimationPlayer = $AnimationPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,6 +26,7 @@ func _physics_process(delta: float) -> void:
 func fly() -> void:
 	if Input.is_action_just_pressed("fly") == true:
 		velocity.y = POWER
+		anim_player.play("power")
 
 func die() -> void:
 	anim_sprite.stop()
