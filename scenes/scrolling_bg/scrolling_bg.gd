fc6032d0d179ac301bb74ed2d8d1cf7a6df442ae
Niamh <niamh.linguist@gmail.com>
2024-11-21 18:01:17 +0200

    parallax

---
+++ scenes/scrolling_bg/scrolling_bg.gd
@@ -0,0 +1,11 @@
+extends ParallaxBackground
+
+func _ready() -> void:
+	SignalManager.on_plane_died.connect(on_plane_died)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	scroll_offset.x -= GameManager.SCROLL_SPEED * delta
+
+func on_plane_died() -> void:
+	set_process(false)
