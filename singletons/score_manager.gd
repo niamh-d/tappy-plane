b25124fcfebdbc55182a52318cc478e45183dd3b
Niamh <niamh.linguist@gmail.com>
2024-11-24 22:23:07 +0200

    refactoring

--- singletons/score_manager.gd
+++ singletons/score_manager.gd
@@ -17,16 +17,24 @@ func get_score() -> int:
 func get_high_score() -> int:
 	return _high_score
 
-func set_hs_data() -> void:
-	var data = high_scores_array[0]
-	_high_score = data["highScore"]
-	hs_player_name = data["playerName"]
-
 func get_hs_player_name() -> String:
 	return hs_player_name
 
 func get_hs_data() -> Array:
 	return high_scores_array
+
+func set_third_highest_score() -> void:
+		if(high_scores_array.size() >= 3):
+			_third_highest_score = high_scores_array[2]["highScore"]
+
+func set_score_data() -> void:
+	set_hs_data()
+	set_third_highest_score()
+
+func set_hs_data() -> void:
+	var data = high_scores_array[0]
+	_high_score = data["highScore"]
+	hs_player_name = data["playerName"]
 
 func set_score(val: int) -> void:
 	_score = val
@@ -43,25 +51,16 @@ func sort_by_score(a: Dictionary, b: Dictionary) -> bool:
 	
 func sort_high_scores_array() -> void:
 	high_scores_array.sort_custom(sort_by_score)
+	
+func insert_new_high_score(name: String) -> void:
+	var new_entry = {"playerName": name, "highScore": _score}
+	high_scores_array.insert(high_scores_array.bsearch_custom(new_entry, sort_by_score), new_entry)
 
 func update_high_scores_array(name: String) -> void:
-	var new_entry = {"playerName": name, "highScore": _score}
 	sort_high_scores_array()
-	high_scores_array.insert(high_scores_array.bsearch_custom(new_entry, sort_by_score), new_entry)
-	set_hs_data()
-	set_third_highest_score()
+	insert_new_high_score(name)
+	set_score_data()
 	SignalManager.on_leaderboard_updated.emit()
-
-func set_third_highest_score() -> void:
-		if(high_scores_array.size() >= 3):
-			_third_highest_score = high_scores_array[2]["highScore"]
-
-func set_score_data() -> void:
-	sort_high_scores_array()
-	var hs_data: Dictionary = high_scores_array[0]
-	_high_score = hs_data["highScore"]
-	hs_player_name = hs_data["playerName"]
-	set_third_highest_score()
 
 func load_high_score_from_file() -> void:
 	var file: FileAccess = FileAccess.open(SCORES_PATH, FileAccess.READ)
@@ -70,6 +69,7 @@ func load_high_score_from_file() -> void:
 			var json = JSON.new()
 			var data = file.get_as_text()
 			high_scores_array = json.parse_string(data)
+			sort_high_scores_array()
 			set_score_data()
 			print("Loaded HS")
 		else:
