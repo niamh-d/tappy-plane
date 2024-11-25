9896a780df3dd74beeab135051e8cea38532a41f
Niamh <niamh.linguist@gmail.com>
2024-11-25 12:18:01 +0200

    complex transition (fade)

--- singletons/game_manager.gd
+++ singletons/game_manager.gd
@@ -4,6 +4,8 @@ const MAIN: PackedScene = preload("res://scenes/main/main.tscn")
 const GAME: PackedScene = preload("res://scenes/game/game.tscn")
 const SIMPLE_TRANSITION: PackedScene = preload("res://scenes/simple_transition/simple_transition.tscn")
 
+const COMPLEX_TRANSITION = preload("res://scenes/complex_transition/complex_transition.tscn")
+
 const SCROLL_SPEED: float = 120.0
 const GROUP_PLANE: String = "Plane"
 
@@ -11,7 +13,8 @@ var next_scene: PackedScene
 
 func load_next_scene(ns: PackedScene) -> void:
 	next_scene = ns
-	get_tree().change_scene_to_packed(SIMPLE_TRANSITION)
+	var cxt = COMPLEX_TRANSITION.instantiate()
+	add_child(cxt)
 
 func load_game_scene() -> void:
 	load_next_scene(GAME)
